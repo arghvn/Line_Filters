f275370f30bb765952d61920d32c844837a663c4
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-17 22:54:37 +0430

    comments

--- main.go
+++ main.go
@@ -7,10 +7,14 @@ import (
 	"strings"
 )
 
+// Wrapping the unbuffered os.Stdin with a buffered scanner gives us a convenient Scan method that advances
+//  the scanner to the next token; which is the next line in the default scanner.
+
 func main() {
 
 	scanner := bufio.NewScanner(os.Stdin)
 
+	// Text returns the current token, here the next line, from the input.
 	for scanner.Scan() {
 
 		ucl := strings.ToUpper(scanner.Text())
@@ -23,3 +27,5 @@ func main() {
 		os.Exit(1)
 	}
 }
+
+// Check for errors during Scan. End of file is expected and not reported by Scan as an error.
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+A line filter is a common type of program that reads input on stdin, processes it,
+ and then prints some derived result to stdout. grep and sed are common line filters.
+
+ Here’s an example line filter in Go that writes a capitalized version of all input text.
+  You can use this pattern to write your own Go line filters.
